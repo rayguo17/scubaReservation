05f53c3af5ef658a407fc2562736217e6035788e
david Cheung <davidcheung888@hotmail.com>
2021-06-04 14:54:40 +0800

    david frontend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handlebars = require("express-handlebars");
 require('dotenv').config();
 const redis=require('redis');
 const redisClient = redis.createClient({
@@ -19,9 +20,50 @@ setupSession(app,redisClient);
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.engine(
+    "handlebars",
+    handlebars({ defaultLayout: "main" })
+);
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 //set up routes for each feature 
 app.use('/',new ViewRouter().router());
+app.get("/", (request, response) => {
+    response.render("index");
+});
+app.get("/fundiving", (request, response) => {
+    response.render("fundiving");
+});
+
+app.get("/coursesJunior", (request, response) => {
+  response.render("coursesJunior");
+});
+
+app.get("/coursesBeginner", (request, response) => {
+  response.render("coursesBeginner");
+});
+
+app.get("/coursesAdvanced", (request, response) => {
+  response.render("coursesAdvanced");
+});
+
+app.get("/coursesSpecialty", (request, response) => {
+  response.render("coursesSpecialty");
+});
+
+app.get("/pool", (request, response) => {
+  response.render("pool");
+});
+app.get("/instructor", (request, response) => {
+    response.render("instructor");
+});
+app.get("/gallery", (request, response) => {
+  response.render("gallery");
+});
+app.get("/faq", (request, response) => {
+    response.render("faq");
+});
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}...`);
